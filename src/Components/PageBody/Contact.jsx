4976de6cfa72ddf661ccaea7540810315f53e94f
Mit Mehta <mitrmehta20@gmail.com>
2025-06-07 14:29:22 -0400

    Lab1 edit

--- src/Components/PageBody/Contact.jsx
+++ src/Components/PageBody/Contact.jsx
@@ -4,7 +4,8 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 const Contact = () => {
   return (
     <div className='centerContainer text-bg-secondary p-3'>
-      Contact
+      <h1>Contact</h1>
+      <p>Email: mmehta7@students.kennesaw.edu</p>
       </div>
   )
 }
---
+++ src/Components/PageBody/ProjectComponents/Lab1.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Lab1 = () => {
+  return (
+    <div className='centerContainer text-bg-secondary p-3'>
+        <h1>Lab 1</h1>
+        <p>Repository: <a href='https://github.com/mitmehta20/IT4403_Project' target='_blank' style={{color:'white'}}><b>IT4403 Project Repo</b></a></p>
+        <p>Lab 1 Repository: <a href='https://github.com/mitmehta20/IT4403_Project/tree/Lab1' target='_blank' style={{color:'white'}}><b>IT4403 Project Repo</b></a></p>
+    </div>
+  )
+}
+
+export default Lab1
